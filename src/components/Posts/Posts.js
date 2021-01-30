02a4da53e69d5b29091350fd4422721cea2048cb
Jasyd Caballero <jasydavid45@gmail.com>
2021-01-29 19:12:49 -0500

    added styles with matherial-ui

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,9 @@
 import Post from "./Post/Post.js";
 
+import useStyles from "./styles.js";
+
 const Posts = () => {
+  const classes = useStyles();
   return (
     <>
       <h1>Posts</h1>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
 import App from "./App";
 
