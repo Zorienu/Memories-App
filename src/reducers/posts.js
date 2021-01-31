2ac152e7cfe683572d4c5301cc1a73483fa1eb02
Jasyd Caballero <jasydavid45@gmail.com>
2021-01-30 23:06:21 -0500

    adding reducers

---
+++ src/reducers/posts.js
@@ -0,0 +1,13 @@
+// state is the same as posts, const reducer
+const reducer = (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL": // fetching all the posts
+      return posts;
+    case "CREATE":
+      return posts;
+    default:
+      return posts;
+  }
+};
+
+export default reducer;
