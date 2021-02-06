59e033068f3c5bcfbb6e2db818420e5c46ee60b0
Jasyd Caballero <jasydavid45@gmail.com>
2021-02-06 15:04:24 -0500

    solved redux user bug {}

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const API = axios.create({ baseURL: "http://localhost:5000" });
-const API = axios.create({ baseURL: "https://memories-using-react.herokuapp.com/posts" });
+const API = axios.create({ baseURL: "https://memories-using-react.herokuapp.com" });
 //const url = "https://memories-using-react.herokuapp.com/posts";
 
 API.interceptors.request.use((req) => {
